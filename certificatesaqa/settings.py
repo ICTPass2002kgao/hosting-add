130352d88985eef2ea944814373037883fce002c
Kgaogelo <kgaogelojoseph33@gmail.com>
2025-01-21 05:49:59 -0800

    added mysql package

--- certificatesaqa/settings.py
+++ certificatesaqa/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 
 from pathlib import Path
+import pymysql
 import django_heroku
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+pymysql.install_as_MySQLdb()
 
 
 # Quick-start development settings - unsuitable for production
