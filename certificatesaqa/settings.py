495d33b363c52d977f3b12e481497c220eabf654
Kgaogelo <kgaogelojoseph33@gmail.com>
2025-06-26 13:05:49 +0200

    Add import_certificates command

--- certificatesaqa/settings.py
+++ certificatesaqa/settings.py
@@ -77,16 +77,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'certificatesaqa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'certificate_db',
-    'USER': 'root',
-    'HOST': 'switchback.proxy.rlwy.net',
-    'PASSWORD': 'woGXDTIUuoeNSWlCkzOuwIcTRimtSdkG',
-    'PORT': '52642', 
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 # SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
