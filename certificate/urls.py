ad62f0a83e2c0adf0e0efc821d7240bd4feb947c
Kgaogelo Mthimkhulu <kgaogelomthimkhulu@Kgaogelos-MacBook-Air.local>
2025-11-20 11:39:02 +0200

    added login

--- certificate/urls.py
+++ certificate/urls.py
@@ -4,17 +4,17 @@ from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
- 
-urlpatterns = [ # Use `str` for name
-    path('logout/',  views.user_logout, name='logout'), 
-    path('upload/', views.upload_certificate, name='upload_certificate'),
-    path('<str:certificate_name>/', views.view_certificate, name='view_certificate'),
+
+
+urlpatterns = [
     path('',  views.superuser_login, name='login'),
+    path('upload/', views.upload_certificate, name='upload_certificate'),
     
+    # MOVED UP: Specific paths must come BEFORE the catch-all path
+    path('logout/',  views.user_logout, name='logout'), 
+
+    # LAST: This catches anything else (like UUIDs)
+    path('<str:certificate_name>/', views.view_certificate, name='view_certificate'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-
